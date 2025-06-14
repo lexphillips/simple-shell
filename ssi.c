7749a33591d3da65b8f56eb2b298c6bc28101395
Lex <lexph@uvic.ca>
2025-06-14 08:24:35 -0700

    First push

---
+++ ssi.c
@@ -0,0 +1,19 @@
+/**
+ * @file ssi.c
+ * @brief A simple shell interpreter
+ *
+ * 
+ * @author lexph
+ * @date June 14, 2025
+ * 
+ * @section Acknowledgment
+*/
+
+
+
+int main() {
+    
+
+
+    return EXIT_SUCCESS;
+}
